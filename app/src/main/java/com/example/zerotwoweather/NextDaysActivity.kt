685c34abb8b72618008720af41110898ef0ebc19
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-08-03 23:06:48 -0300

    gradle version update and add next days activity

---
+++ app/src/main/java/com/example/zerotwoweather/NextDaysActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zerotwoweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextDaysActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_days)
+    }
+}
