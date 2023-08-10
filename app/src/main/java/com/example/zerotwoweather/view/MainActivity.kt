adce0ed70ae3ddca1d642263065dc12acac1abc3
Guilherme Pecanha <gpgaddy@outlook.com>
2023-08-09 22:12:15 -0300

    recyclerview test

--- app/src/main/java/com/example/zerotwoweather/view/MainActivity.kt
+++ app/src/main/java/com/example/zerotwoweather/view/MainActivity.kt
@@ -2,11 +2,74 @@ package com.example.zerotwoweather.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.zerotwoweather.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
