977db795e2397fff968a0720aeac0890a91f0d49
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-08-11 21:42:53 -0300

    Created Base for API Implementation.

--- app/src/main/java/com/example/zerotwoweather/view/MainActivity.kt
+++ app/src/main/java/com/example/zerotwoweather/view/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.zerotwoweather.R
+import com.example.zerotwoweather.view.remote.RetrofitModule
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var rvTempList: RecyclerView
     lateinit var cardAdapter: AdapterCardTemp
+
+    private val retrofitModule = RetrofitModule
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,26 +39,9 @@ class MainActivity : AppCompatActivity() {
 
         cardAdapter.submitList(cardTempList)
 
-
-
+//        val weatherService = retrofitModule.createWeatherService()
+//
+////        weatherService
+////            .getWeatherData()
     }
-
-    class WeatherActivity : AppCompatActivity() {
-        // ...
-
-        private fun fetchWeatherData(latitude: Double, longitude: Double) {
-            val apiKey = "SUA_CHAVE_DE_API_AQUI"
-
-            CoroutineScope(Dispatchers.IO).launch {
-                val response = RetrofitClient.openMeteoApiService.getWeatherData(latitude, longitude, "yes", apiKey)
-                if (response.isSuccessful) {
-                    val openMeteoResponse = response.body()
-                    // Atualize a interface com os dados do openMeteoResponse
-                } else {
-                    // Trate os erros
-                }
-            }
-        }
-    }
-
 }
